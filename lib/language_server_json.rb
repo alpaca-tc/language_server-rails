e28b1df1f77b2a5f11af9068d94f878d1fe4ad2d
alpaca-tc <alpaca-tc@alpaca.tc>
2017-09-25 20:24:43 +0900

    Add logger

--- lib/language_server_json.rb
+++ lib/language_server_json.rb
@@ -5,4 +5,5 @@ require 'language_server_json/version'
 
 module LanguageServerJson
   autoload :Application, 'language_server_json/application'
+  autoload :Service, 'language_server_json/service'
 end
