9a4e9d53ba4a192bbc0f4a347be76449d6afec78
alpaca-tc <alpaca-tc@alpaca.tc>
2017-10-28 17:14:08 +0900

    Find content and node

--- lib/language_server_rails/content_context.rb
+++ lib/language_server_rails/content_context.rb
@@ -14,23 +14,38 @@ module LanguageServerRails
 
     def cursor_text
       location = cursor_node.location
-      lines[line_no - 1][location.keyword.column...location.keyword.last_column]
-    end
+      line = parsed.location.expression.source_buffer.source_lines[line_no - 1]
 
-    private
+      case cursor_node.type
+      when :module
+        line[location.keyword.column...location.keyword.last_column]
+      when :const
+        line[location.column...location.last_column]
+      else
+        binding.pry
+      end
+    end
 
     def cursor_node
       @cursor_node ||= find_node(parsed)
     end
 
+    private
+
     def find_node(node)
+      return unless node
+
       location = node.location
 
-      if location.first_line == line_no && (location.keyword.column..location.keyword.last_column).include?(character_no)
+      if location.first_line == line_no && (location.expression.column..location.expression.last_column).include?(character_no)
         return node
-      else
-        binding.pry
+      elsif (location.first_line..location.last_line).include?(line_no)
+        node.children.find do |child_node|
+          find_node(child_node)
+        end
       end
+    rescue
+      binding.pry
     end
 
     def parsed
--- lib/language_server_rails/service/hover_service.rb
+++ lib/language_server_rails/service/hover_service.rb
@@ -6,7 +6,10 @@ module LanguageServerRails
       def do_hover
         uri_str = params.dig(:params, :textDocument, :uri)
         content = Content.new(uri_str)
+        content_context = ContentContext.new(content, params.dig(:params, :position, :line), params.dig(:params, :position, :character))
         binding.pry
+        content_context.cursor_text
+        content_context.cursor_node
 
         Interface::Hover.new(
           contents: 'hello contents',
