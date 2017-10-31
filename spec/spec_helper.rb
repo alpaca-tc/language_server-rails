34327f9353dd64fc6e88f86f7b257affb526d46e
alpaca-tc <alpaca-tc@alpaca.tc>
2017-10-31 19:29:46 +0900

    Enable focus tag

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = '.rspec_status'
   config.disable_monkey_patching!
 
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
